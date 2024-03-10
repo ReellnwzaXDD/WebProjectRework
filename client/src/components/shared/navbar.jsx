e4e027d7ba0db81c2dea1c89484f4b3b37a7f2dc
ReellnwzaXDD <patanin511@gmail.com>
2024-03-10 12:26:11 +0700

    edit navbar

--- client/src/components/shared/navbar.jsx
+++ client/src/components/shared/navbar.jsx
@@ -145,8 +145,8 @@ const Navbar = () => {
             <li>
               <Link to="/summary">OrderSummary</Link>
             </li>
-            {isLoggedIn ? (<li><Link to="/logout">Logout</Link></li>):(
-              <li><Link to="/login">Login</Link></li>
+            {isLoggedIn ? (<li><Link to="/login">Login</Link></li>):(
+              <li><Link to="/logout">Logout</Link></li>
             )}
             
           </ul>
